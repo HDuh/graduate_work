a4dca32d9c1151b2192b3e70348ef9d5e6ad3201
Nikita Dushkov <n.dushkov@yadro.com>
2022-12-19 12:42:56 +0300

    добавил в env файл переменную с моделями, чтобы не терялись при миграции

--- order_service/src/migrations/env.py
+++ order_service/src/migrations/env.py
@@ -6,9 +6,11 @@ from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import AsyncEngine
-from src.db.base import Base
-from src.db.models import *
 
+import src.db.models as models
+from src.db.base import Base
+
+models_for_migrations = models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
