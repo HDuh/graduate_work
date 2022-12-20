f53bc01b1e15c9c94018f6c7470ba06adf54d0a3
Sergey Aleshin <ageresnishela@gmail.com>
2022-12-20 20:43:41 +0300

    Удалил лишка

--- order_service/src/schemas/customer.py
+++
@@ -1,5 +0,0 @@
-from src.schemas.base import BaseSchema
-
-
-class CustomerSchema(BaseSchema):
-    id: str
--- order_service/src/schemas/order.py
+++
@@ -1,7 +0,0 @@
-from src.schemas.base import BaseSchema
-
-
-class OrderCreate(BaseSchema):
-    customer_id: str
-    price_id: str
-    quantity: int
